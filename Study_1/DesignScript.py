2a6e7eb7992e2b818f74b5aab2a8884246ec6c77
Gin18062003 <nguyenminhhieu18062003@gmail.com>
2024-08-02 16:43:23 +0700

    Lesson_03

--- Study_1/DesignScript.py
+++ Study_1/DesignScript.py
@@ -1,8 +1,48 @@
-items =IN[1]
+"""Copyright(c) 2024 by: nguyenminhhieu18062003@gmail.com"""
+import clr 
+import sys
+sys.path.append(r'C:\Program Files\Autodesk\Revit 2023\AddIns\DynamoForRevit\IronPython.StdLib.2.7.8')
+import math 
+from System.Collections.Generic import *
 
-result = []
-for num in range(0,len(items),1):
-    if num %2:
-        result.append(items[num])
+clr.AddReference("ProtoGeometry")
+from Autodesk.DesignScript.Geometry import *
 
-OUT =  result
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import*
+from  Autodesk.Revit.UI.Selection import*
+
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+view = doc.ActiveView
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+
+#Preparing input from dynamo to revit
+elements = UnwrapElement(IN[0])
+
+def getDBLineByLines(elements):
+    re = []
+    for i in elements:
+        crv = i.GeometryCurve
+        re.append(crv)
+    return re
+
+factor = IN[1]
+dbLines = getDBLineByLines(element)
+
+points = [dbLine.Evaluate(factor, True).ToPoint() for dbLine in dbLines]
+
+OUT = points
---
+++ Study_1/script.py
@@ -0,0 +1,11 @@
+num = 4.5 #float
+num = 4 #int
+name = "mark"  #str
+objects = [
+    {
+        "name" : "nguyen van A",
+        "phone_number" : "0339001600"
+    }
+]
+
+print(type(objects))
